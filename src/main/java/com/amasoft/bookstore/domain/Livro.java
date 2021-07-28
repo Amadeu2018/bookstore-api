91d3be56ed4634b1e0857eb0066b2df1c9eb2a16
Amadeu2018 <amadeualexandrealeg@gmail.com>
2021-07-28 13:50:29 +0100

    Finalizando as classes de dominio

--- src/main/java/com/amasoft/bookstore/domain/Livro.java
+++ src/main/java/com/amasoft/bookstore/domain/Livro.java
@@ -1,20 +1,26 @@
 package com.amasoft.bookstore.domain;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Livro {
+public class Livro implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String titulo;
     private String nome_autor;
     private String texto;
 
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 
     public Livro(){
         super();
     }
-
     public Livro(Integer id, String titulo, String nome_autor, String texto, Categoria categoria) {
         this.id = id;
         this.titulo = titulo;
