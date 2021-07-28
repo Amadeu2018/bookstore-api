20592f20e67425b626c069f5381a47411c9e86e1
Amadeu2018 <amadeualexandrealeg@gmail.com>
2021-07-28 14:08:15 +0100

    repositories implementados

---
+++ src/main/java/com/amasoft/bookstore/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.amasoft.bookstore.repositories;
+
+import com.amasoft.bookstore.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+}
---
+++ src/main/java/com/amasoft/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.amasoft.bookstore.repositories;
+
+import com.amasoft.bookstore.domain.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer>{
+}
