a77c0079e4e80773af83993ed9faccf4fe24c199
Amadeu2018 <amadeualexandrealeg@gmail.com>
2021-08-02 11:50:29 +0100

    Excecao personalizada para objecto não encontrado!

--- src/main/java/com/amasoft/bookstore/service/CategoriaService.java
+++ src/main/java/com/amasoft/bookstore/service/CategoriaService.java
@@ -2,6 +2,7 @@ package com.amasoft.bookstore.service;
 
 import com.amasoft.bookstore.domain.Categoria;
 import com.amasoft.bookstore.repositories.CategoriaRepository;
+import com.amasoft.bookstore.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,7 @@ public class CategoriaService {
 
     public Categoria findById(Integer id) {
         Optional<Categoria> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objecto não encontrado! id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 }
---
+++ src/main/java/com/amasoft/bookstore/service/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.amasoft.bookstore.service.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.ServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException e, ServletRequest request) {
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
+                e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+}
