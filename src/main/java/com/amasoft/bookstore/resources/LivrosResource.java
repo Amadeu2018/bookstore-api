f2f3946dd32ff761dedbfbaf2c1ab9234606cc67
Amadeu2018 <amadeualexandrealeg@gmail.com>
2021-08-02 16:36:16 +0100

    GET para Livro por ID

---
+++ src/main/java/com/amasoft/bookstore/resources/LivrosResource.java
@@ -0,0 +1,24 @@
+package com.amasoft.bookstore.resources;
+
+import com.amasoft.bookstore.domain.Livro;
+import com.amasoft.bookstore.service.LivroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/livros")
+public class LivrosResource {
+
+    @Autowired
+    private LivroService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Livro> findById(@PathVariable Integer id) {
+        Livro obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ src/main/java/com/amasoft/bookstore/service/LivroService.java
@@ -0,0 +1,22 @@
+package com.amasoft.bookstore.service;
+
+import com.amasoft.bookstore.domain.Livro;
+import com.amasoft.bookstore.repositories.LivroRepository;
+import com.amasoft.bookstore.service.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class LivroService {
+
+    @Autowired
+    private LivroRepository repository;
+
+    public Livro findById(Integer id) {
+        Optional<Livro> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! id: " + id + ", Tipo: " + Livro.class.getName()));
+    }
+}
